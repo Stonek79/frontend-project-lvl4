77edbe5863cf0cbd2feba9c026e80a77fc204161
Stonek79 <amit-yurist@mail.ru>
2021-03-24 21:24:48 +0300

    Try fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // @ts-check
 
+import gon from 'gon';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import run from './init';
 import '../assets/application.scss';
-import gon from 'gon';
 
 run(gon);
