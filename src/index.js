e0c5652ef7beb47606d08a4697e275b588df1e3e
Stonek79 <amit-yurist@mail.ru>
2021-03-24 20:09:15 +0300

    Fix package

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import run from './init';
 import '../assets/application.scss';
-
 import gon from 'gon';
-
-if (process.env.NODE_ENV !== 'production') {
-  localStorage.debug = 'chat:*';
-}
 
 run(gon);
