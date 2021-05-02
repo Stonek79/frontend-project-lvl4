ebf418ef26f0326dcc2e45ea58e265799cafac3d
Stonek79 <amit-yurist@mail.ru>
2021-05-02 14:17:07 +0300

    Some fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-new */
 // @ts-check
 
 import ReactDOM from 'react-dom';
+import Rollbar from 'rollbar';
 import { io } from 'socket.io-client';
 
 import 'core-js/stable/index.js';
@@ -10,18 +12,26 @@ import '../assets/application.scss';
 import run from './init.jsx';
 
 const start = async () => {
-  const env = process.env.NODE_ENV;
-  if (env !== 'production') {
+  const envy = process.env.NODE_ENV;
+
+  new Rollbar({
+    accessToken: process.env.ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    enabled: envy === 'production',
+  });
+
+  if (envy !== 'production') {
     localStorage.debug = 'chat:*';
   }
 
-  const socket = io();
+  const url = window.location.origin;
+  const socket = io(url);
 
   const virtualDom = await run(socket);
   const element = document.getElementById('chat');
 
-  const { render } = ReactDOM;
-  render(virtualDom, element);
+  ReactDOM.render(virtualDom, element);
 };
 
 start();
