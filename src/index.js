b72e26567c72e82676aae590e698f169bb60ab73
Stonek79 <amit-yurist@mail.ru>
2021-05-02 14:04:00 +0300

    Some fix

--- src/index.js
+++ src/index.js
@@ -9,15 +9,19 @@ import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
 import run from './init.jsx';
 
-const env = process.env.NODE_ENV;
-if (env !== 'production') {
-  localStorage.debug = 'chat:*';
-}
+const start = async () => {
+  const env = process.env.NODE_ENV;
+  if (env !== 'production') {
+    localStorage.debug = 'chat:*';
+  }
 
-const socket = io();
+  const socket = io();
 
-const virtualDom = run(socket);
-const element = document.getElementById('chat');
+  const virtualDom = await run(socket);
+  const element = document.getElementById('chat');
 
-const { render } = ReactDOM;
-render(virtualDom, element);
+  const { render } = ReactDOM;
+  render(virtualDom, element);
+};
+
+start();
