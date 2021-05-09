7b1c695c35af97d020d2e2f19b8d44c263840f6d
Stonek79 <amit-yurist@mail.ru>
2021-05-09 21:52:24 +0300

    Try fix err

--- src/init.jsx
+++ src/init.jsx
@@ -2,19 +2,18 @@
 import React from 'react';
 // import axios from 'axios';
 import { configureStore } from '@reduxjs/toolkit';
-import { I18nextProvider, initReactI18next } from 'react-i18next';
+// import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 
 import App from './components/App.jsx';
 import AppContext from './context/AppContext.jsx';
-import resources from './resources/resources.js';
 import rootReducer from './slices/index.js';
 // import routes from './routes';
 import { addMessage } from './slices/messageSlice.js';
 import {
   addChannel, removeChannel, renameChannel, updateChannels,
 } from './slices/channelSlice.js';
-import i18next from './i18n.js';
+// import i18next from './i18n.js';
 // import './i18n.js';
 
 const getAuthHeader = () => {
@@ -30,7 +29,7 @@ const getAuthHeader = () => {
 };
 
 console.log('init module');
-const i18n = i18next.createInstance();
+// const i18n = i18next.createInstance();
 
 const Init = async (socket) => {
   console.log('init 1');
@@ -40,13 +39,6 @@ const Init = async (socket) => {
   });
 
   console.log('init 2');
-  await i18n
-    .use(initReactI18next)
-    .init({
-      lng: 'ru',
-      debug: false,
-      resources,
-    });
 
   // const i18n = await i18next;
 
